609494fbba1457a39aab3f3724e1233e13a232f4
Juan <jdteheran@uninorte.edu.co>
2021-09-28 08:32:04 -0500

    mejore realizada y finalizada

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 numero = int(input('pasa el numero: '))
 
 cont = 0
-for i in range(1, numero + 1):
+for i in range(2, numero + 1):
     if numero % i == 0:
         cont = cont + 1
 
-if cont == 2:
+if cont == 1:
     print('el numero ', numero, ' es Primo')
 else:
     print('el numero ', numero, ' NO es Primo')
